be497c7921b5dc22d113c06948bc97c9203ab61e
Michael Nash <michael@spatialnetworks.com>
2019-02-15 16:16:19 -0500

    record data points

--- MLKits/plinko/score.js
+++ MLKits/plinko/score.js
@@ -1,5 +1,8 @@
+const outputs = [];
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-  // Ran every time a balls drops into a bucket
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
+  console.log(outputs);
 }
 
 function runAnalysis() {
