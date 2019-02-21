5c2f50e0e96bf973cfd433232014f0cdb06b9b80
Michael Nash <michael@spatialnetworks.com>
2019-02-21 15:35:26 -0500

    accuracy predictor

--- MLKits/plinko/score.js
+++ MLKits/plinko/score.js
@@ -1,5 +1,4 @@
 const outputs = [];
-const predictionPoint = 300;
 const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
@@ -7,8 +6,27 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 }
 
 function runAnalysis() {
-  const bucket = _.chain(outputs)
-    .map(row => [distance(row[0]), row[3]])
+  const testSetSize = 10;
+  const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
+
+  let numberCorrect = 0;
+  for (let i = 0; i < testSet.length; i++) {
+    const bucket = knn(trainingSet, testSet[i][0]);
+    if (bucket === testSet[i][3]) {
+      numberCorrect++;
+    }
+  }
+
+  console.log('Accuracy: ', numberCorrect / testSetSize);
+}
+
+function distance(pointA, pointB) {
+  return Math.abs(pointA - pointB);
+}
+
+function knn(data, point) {
+  return _.chain(data)
+    .map(row => [distance(row[0], point), row[3]])
     .sortBy(row => row[0])
     .slice(0, k)
     .countBy(row => row[1])
@@ -17,9 +35,12 @@ function runAnalysis() {
     .first()
     .parseInt()
     .value();
-  console.log('Your point will probably fall into', bucket);
 }
 
-function distance(point) {
-  return Math.abs(point - predictionPoint);
+function splitDataset(data, testCount) {
+  const shuffled = _.shuffle(data);
+  const testSet = _.slice(shuffled, 0, testCount);
+  const trainingSet = _.slice(shuffled, testCount);
+
+  return [testSet, trainingSet];
 }
