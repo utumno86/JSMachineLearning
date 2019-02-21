51d29a3c7c549a0ad7273c58ce5bcf6b1ccadc7d
Michael Nash <michael@spatialnetworks.com>
2019-02-21 16:12:09 -0500

    refactor

--- MLKits/plinko/score.js
+++ MLKits/plinko/score.js
@@ -9,15 +9,13 @@ function runAnalysis() {
   const testSetSize = 10;
   const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
 
-  let numberCorrect = 0;
-  for (let i = 0; i < testSet.length; i++) {
-    const bucket = knn(trainingSet, testSet[i][0]);
-    if (bucket === testSet[i][3]) {
-      numberCorrect++;
-    }
-  }
+  const accuracy = _.chain(testSet)
+    .filter(testPoint => knn(trainingSet, testPoint[0]) === testPoint[3])
+    .size()
+    .divide(testSetSize)
+    .value();
 
-  console.log('Accuracy: ', numberCorrect / testSetSize);
+  console.log('Accuracy: ', accuracy);
 }
 
 function distance(pointA, pointB) {
