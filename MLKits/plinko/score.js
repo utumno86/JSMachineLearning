bc7105b39b6471cf98307379107cf81b28e1a3db
Michael Nash <michael@spatialnetworks.com>
2019-02-25 12:19:04 -0500

    contoled for bounciness

--- MLKits/plinko/score.js
+++ MLKits/plinko/score.js
@@ -8,9 +8,9 @@ function runAnalysis() {
   const testSetSize = 100;
   const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
 
-  _.range(1, 20).forEach(k => {
+  _.range(1, 20).forEach((k) => {
     const accuracy = _.chain(testSet)
-      .filter(testPoint => knn(trainingSet, testPoint[0], k) === testPoint[3])
+      .filter(testPoint => knn(trainingSet, _.initial(testPoint), k) === testPoint[3])
       .size()
       .divide(testSetSize)
       .value();
@@ -20,16 +20,27 @@ function runAnalysis() {
 }
 
 function distance(pointA, pointB) {
-  return Math.abs(pointA - pointB);
+  return _.chain(pointA)
+    .zip(pointB)
+    .map(([a, b]) => (a - b) ** 2)
+    .sum()
+    .value() ** 0.5;
 }
 
 function knn(data, point, k) {
+  // point has three values
   return _.chain(data)
-    .map(row => [distance(row[0], point), row[3]])
+    .map((row) => {
+      return [
+        distance(_.initial(row), _.initial(point)),
+        _.last(row),
+      ];
+    })
     .sortBy(row => row[0])
     .slice(0, k)
     .countBy(row => row[1])
     .toPairs()
+    .sortBy(row => row[1])
     .last()
     .first()
     .parseInt()
