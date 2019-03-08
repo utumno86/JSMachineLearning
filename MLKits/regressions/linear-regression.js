1bb04dfaffb817a229c22d2f38e1887f175abd2e
Michael Nash <michael@spatialnetworks.com>
2019-03-08 14:55:03 -0500

    linear regressions v1

--- MLKits/regressions/linear-regression.js
+++ MLKits/regressions/linear-regression.js
@@ -22,11 +22,14 @@ class LinearRegression {
 
     const bSlope = _.sum(currentGuessesForMPG.map((guess, i) => {
       return guess - this.labels[i][0];
-    })) * 2 / this.features.lenngth;
+    })) * 2 / this.features.length;
 
-    const mSlope = currentGuessesForMPG.map(guess, i) => {
-      
-    }
+    const mSlope = (_.sum(currentGuessesForMPG.map((guess, i) => {
+      return -1 * this.features[i][0] * (this.labels[i][0] - guess);
+    })) * 2) / this.features.length;
+
+    this.m = this.m - mSlope * this.options.learningRate;
+    this.b = this.b - bSlope * this.options.learningRate;
   }
 
   train() {
