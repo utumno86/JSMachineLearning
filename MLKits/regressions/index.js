d8bd8ec10d5b2dba1185f2f142181d0c69b08290
Michael Nash <michael@spatialnetworks.com>
2019-03-11 17:16:02 -0400

    regression with multiple factors

--- MLKits/regressions/index.js
+++ MLKits/regressions/index.js
@@ -6,12 +6,12 @@ const LinearRegression = require('./linear-regression');
 let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
   shuffle: true,
   splitTest: 50,
-  dataColumns: ['horsepower'],
+  dataColumns: ['horsepower', 'displacement', 'weight'],
   labelColumns: ['mpg'],
 });
 
 const regression = new LinearRegression(features, labels, {
-  learningRate: 0.00001,
+  learningRate: 0.1,
   iterations: 100,
 });
 
