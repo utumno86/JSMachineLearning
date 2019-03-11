76883ed220e8b18d7120806b130836f0650bf335
Michael Nash <michael@spatialnetworks.com>
2019-03-11 15:49:37 -0400

    standardized the data

--- MLKits/regressions/linear-regression.js
+++ MLKits/regressions/linear-regression.js
@@ -69,9 +69,22 @@ class LinearRegression {
 
   processFeatures(features) {
     features = tf.tensor(features);
-    features = tf.ones([features.shape[0], 1]).concat(features, 1);
+    if (this.mean && this.variance) {
+      features = features.sub(this.mean).div(this.variance.pow(0.5));
+    } else {
+      features = this.standardize(features);
+    }
 
+    features = tf.ones([features.shape[0], 1]).concat(features, 1);
     return features;
+  }
+
+  standardize(features) {
+    const { mean, variance } = tf.moments(features, 0);
+    this.mean = mean;
+    this.variance = variance;
+
+    return features.sub(mean).div(variance.pow(0.5));
   }
 }
 
