57f8cc1d9c106f17754d94729cb9a9f9f0d04fc3
Michael Nash <michael@spatialnetworks.com>
2019-03-12 14:13:55 -0400

    batch gradient descent

--- MLKits/regressions/linear-regression.js
+++ MLKits/regressions/linear-regression.js
@@ -9,20 +9,21 @@ class LinearRegression {
 
     this.options = Object.assign({
       learningRate: 0.1,
-      iterations: 1000,
+      iterations: 3,
+      batchSize: 1,
     }, options);
 
     this.weights = tf.zeros([this.features.shape[1], 1]);
   }
 
-  gradientDescent() {
-    const currentGuesses = this.features.matMul(this.weights);
-    const differences = currentGuesses.sub(this.labels);
+  gradientDescent(features, labels) {
+    const currentGuesses = features.matMul(this.weights);
+    const differences = currentGuesses.sub(labels);
 
-    const slopes = this.features
+    const slopes = features
       .transpose()
       .matMul(differences)
-      .div(this.features.shape[0]);
+      .div(features.shape[0]);
 
     this.weights = this.weights.sub(slopes.mul(this.options.learningRate));
   }
@@ -45,8 +46,16 @@ class LinearRegression {
   // }
 
   train() {
+    const batchQuantity = Math.floor(this.features.shape[0] / this.options.batchSize);
+
     for (let i = 0; i < this.options.iterations; i++) {
-      this.gradientDescent();
+      for (let j = 0; j < batchQuantity; j++) {
+        const startIndex = j * this.options.batchSize;
+        const { batchSize } = this.options;
+        const featureSlice = this.features.slice([startIndex, 0], [batchSize, -1]);
+        const labelSlice = this.labels.slice([startIndex, 0], [batchSize, -1]);
+        this.gradientDescent(featureSlice, labelSlice);
+      }
       this.recordMSE();
       this.updateLearningRate();
     }
