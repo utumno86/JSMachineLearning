b3bf35e18432133649304a98d803eda55e069121
Michael Nash <michael@spatialnetworks.com>
2019-03-13 17:43:49 -0400

    first draft of the regression logic

--- MLKits/regressions/logistic-regression/index.js
+++ MLKits/regressions/logistic-regression/index.js
@@ -1,6 +1,7 @@
 require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
 const loadCSV = require('../data/load-csv');
+const LogisticRegression = require('./logistic-regression');
 
 const { features, labels, testFeatures, testLabels } = loadCSV('../data/cars.csv', {
   dataColumns: [
@@ -13,10 +14,19 @@ const { features, labels, testFeatures, testLabels } = loadCSV('../data/cars.csv
   splitTest: 50,
   converters: {
     passedemissions: value => {
-      console.log(value);
       return value === 'TRUE' ? 1 : 0;
     },
   },
 });
 
-console.log(labels);
+const regression = new LogisticRegression(features, labels, {
+  learningRate: 0.5,
+  iterations: 100,
+  batchSize: 50,
+});
+
+regression.train();
+
+regression.predict([
+  [88, 97, 1.065],
+]).print();
