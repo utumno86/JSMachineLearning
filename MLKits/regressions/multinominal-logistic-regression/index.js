cb2155f40aec6e960ac6ee423a29a05057a7d009
Michael Nash <michael@spatialnetworks.com>
2019-03-15 14:27:40 -0400

    changing from marginal to conditional probability

--- MLKits/regressions/multinominal-logistic-regression/index.js
+++ MLKits/regressions/multinominal-logistic-regression/index.js
@@ -1,8 +1,10 @@
 require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
+const _ = require('lodash');
 const plot = require('node-remote-plot');
 const loadCSV = require('../data/load-csv');
 const LogisticRegression = require('./logistic-regression');
+
 
 const { features, labels, testFeatures, testLabels } = loadCSV('../data/cars.csv', {
   dataColumns: [
@@ -10,31 +12,31 @@ const { features, labels, testFeatures, testLabels } = loadCSV('../data/cars.csv
     'displacement',
     'weight',
   ],
-  labelColumns: ['passedemissions'],
+  labelColumns: ['mpg'],
   shuffle: true,
   splitTest: 50,
   converters: {
-    passedemissions: value => {
-      return value === 'TRUE' ? 1 : 0;
-    },
+    mpg: ((value) => {
+      const mpg = parseFloat(value);
+
+      if (mpg < 15) {
+        return [1, 0, 0];
+      } if (mpg < 30) {
+        return [0, 1, 0];
+      }
+      return [0, 0, 1];
+    }),
   },
 });
 
-const regression = new LogisticRegression(features, labels, {
+
+const regression = new LogisticRegression(features, _.flatMap(labels), {
   learningRate: 0.5,
   iterations: 100,
   batchSize: 10,
-  decisionBoundary: 0.6,
 });
 
 regression.train();
-
 regression.predict([
-  [88, 97, 1.065],
+  [150, 200, 2.223],
 ]).print();
-
-console.log(regression.test(testFeatures, testLabels));
-
-plot({
-  x: regression.costHistory.reverse()
-})
