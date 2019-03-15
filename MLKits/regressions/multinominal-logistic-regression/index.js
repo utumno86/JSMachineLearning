0bec81bbf3df9fb4fed528e869a152ed204a2aa5
Michael Nash <michael@spatialnetworks.com>
2019-03-15 12:17:19 -0400

    multinomial logistic regression

---
+++ MLKits/regressions/multinominal-logistic-regression/index.js
@@ -0,0 +1,40 @@
+require('@tensorflow/tfjs-node');
+const tf = require('@tensorflow/tfjs');
+const plot = require('node-remote-plot');
+const loadCSV = require('../data/load-csv');
+const LogisticRegression = require('./logistic-regression');
+
+const { features, labels, testFeatures, testLabels } = loadCSV('../data/cars.csv', {
+  dataColumns: [
+    'horsepower',
+    'displacement',
+    'weight',
+  ],
+  labelColumns: ['passedemissions'],
+  shuffle: true,
+  splitTest: 50,
+  converters: {
+    passedemissions: value => {
+      return value === 'TRUE' ? 1 : 0;
+    },
+  },
+});
+
+const regression = new LogisticRegression(features, labels, {
+  learningRate: 0.5,
+  iterations: 100,
+  batchSize: 10,
+  decisionBoundary: 0.6,
+});
+
+regression.train();
+
+regression.predict([
+  [88, 97, 1.065],
+]).print();
+
+console.log(regression.test(testFeatures, testLabels));
+
+plot({
+  x: regression.costHistory.reverse()
+})
