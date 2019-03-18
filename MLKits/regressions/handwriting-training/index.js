fdeb4c1a276faa7709671e160193f894e3a6c668
Michael Nash <michael@spatialnetworks.com>
2019-03-18 11:49:02 -0400

    first pass at handwriting analysis

--- MLKits/regressions/handwriting-training/index.js
+++ MLKits/regressions/handwriting-training/index.js
@@ -5,8 +5,29 @@ const plot = require('node-remote-plot');
 const LogisticRegression = require('./logistic-regression');
 const mnist = require('mnist-data');
 
-const mnistData = mnist.training(0, 1);
+const mnistData = mnist.training(0, 1000);
 
 const features = mnistData.images.values.map(image => _.flatMap(image));
-console.log(mnistData.labels.values);
+const encodedLabels = mnistData.labels.values.map(label => {
+  const row = new Array(10).fill(0);
+  row[label] = 1;
+  return row;
+});
 
+const regression = new LogisticRegression(features, encodedLabels, {
+  learningRate: 1,
+  iterations: 5,
+  batchSize: 100,
+})
+regression.train();
+
+const testMnistData = mnist.testing(0, 100);
+const testFeatures = testMnistData.images.values.map(image => _.flatMap(image));
+const testEncodedLabels = testMnistData.labels.values.map(label => {
+  const row = new Array(10).fill(0);
+  row[label] = 1;
+  return row;
+});
+
+const accuracy = regression.test(testFeatures, testEncodedLabels);
+console.log('Accuracy is', accuracy);
