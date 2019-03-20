c541869d9883ef7ae4b0f2ed0e35d15f09f23541
Michael Nash <michael@spatialnetworks.com>
2019-03-20 15:15:37 -0400

    wrapped load data in function

--- MLKits/regressions/handwriting-training/index.js
+++ MLKits/regressions/handwriting-training/index.js
@@ -7,18 +7,25 @@ const mnist = require('mnist-data');
 
 const mnistData = mnist.training(0, 60000);
 
-const features = mnistData.images.values.map(image => _.flatMap(image));
-const encodedLabels = mnistData.labels.values.map(label => {
-  const row = new Array(10).fill(0);
-  row[label] = 1;
-  return row;
-});
+function loadData() {
+  const features = mnistData.images.values.map(image => _.flatMap(image));
+  const encodedLabels = mnistData.labels.values.map(label => {
+    const row = new Array(10).fill(0);
+    row[label] = 1;
+    return row;
+  });
 
-const regression = new LogisticRegression(features, encodedLabels, {
+  return { features, labels: encodedLabels };
+}
+
+const {features, labels } = loadData();
+
+
+const regression = new LogisticRegression(features, labels, {
   learningRate: 1,
   iterations: 20,
   batchSize: 100,
-})
+});
 regression.train();
 
 const testMnistData = mnist.testing(0, 1000);
