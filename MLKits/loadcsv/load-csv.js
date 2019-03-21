3f232d5a5cb99b4a16da08437b1e6ff3675cb5b0
Michael Nash <michael@spatialnetworks.com>
2019-03-21 11:53:37 -0400

    pass in custom converters

--- MLKits/loadcsv/load-csv.js
+++ MLKits/loadcsv/load-csv.js
@@ -1,11 +1,32 @@
 const fs = require('fs');
 const _ = require('lodash')
 
-function loadCSV(fileName, options) {
+function loadCSV(fileName, { converters }) {
   let data = fs.readFileSync(fileName, { encoding: 'utf-8' });
   data = data.split('\n').map(row => row.split(','));
   data = data.map(row => _.dropRightWhile(row, val => val === ''));
+  const headers = _.first(data);
+
+  data = data.map((row, index) => {
+    if (index === 0) {
+      return row;
+    }
+
+    return row.map((element, index) => {
+
+      if (converters[headers[index]]) {
+        const converted = converters[headers[index]](element);
+        return _.isNaN(converted) ? element : converted;
+      }
+      const result = parseFloat(element);
+      return _.isNaN(result) ? element : result;
+    });
+  });
   console.log(data);
 }
 
-loadCSV('data.csv');
+loadCSV('data.csv', {
+  converters: {
+    passed: val => val === 'TRUE',
+  }
+});
